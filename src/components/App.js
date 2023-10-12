0cc027a47bc9640b8803a6d0ca415553da0185e0
SoumyaRanjan63 <sranjan6370@gmail.com>
2023-10-12 23:56:02 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import DisplayRelativeList from './DisplayRelativeList'
 
 const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
+               <DisplayRelativeList />
     </div>
   )
 }
---
+++ src/components/DisplayRelativeList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const DisplayRelativeList = () => {
+    const relativeList=[
+        'abc',
+        'gadg',
+        'fgfgr',
+        'fgfe'
+    ]
+    return (
+        <div>
+            <h1>Diwali visiting list</h1>
+            <ol>
+                {
+                    relativeList.map((relative,index)=>(
+                        <li key={`relativeListItem{index+1}`}>
+                            {relative}
+                        </li>
+                    ))
+                }
+            </ol>
+        </div>
+    );
+}
+
+export default DisplayRelativeList;
